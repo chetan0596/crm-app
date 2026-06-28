e3c6615d11716d0473803a8013e9a5350b9fd5cf
chetan0596 <136224284+chetan0596@users.noreply.github.com>
2026-06-28 20:49:18 +0530

    add proxy configuration for API requests in development server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.atriengineers.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   plugins: [
     react(),
     VitePWA({
